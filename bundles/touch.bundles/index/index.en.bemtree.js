ba90a626be10f5d83a9eaead13a9dd4b0b12c9ab
russianNinja <itolstof@gmail.com>
2019-07-15 23:31:39 +0300

    WIP

--- bundles/touch.bundles/index/index.en.bemtree.js
+++ bundles/touch.bundles/index/index.en.bemtree.js
@@ -3892,6 +3892,66 @@ block("footer").content()(() => {
           }
         }
       ]
+    },
+    {
+      elem: "partners",
+      content: [
+        {
+          block: "link",
+          targer: "_blank",
+          attrs: {
+            rel: "noreferrer, noopener, nofollow"
+          },
+          url: "http://www.energyfm.ru/index_city/kw1/182314/station/84",
+          content: {
+            block: "lazyImage",
+            mix: [{ block: "lazyImage", mods: { hidden: true } }],
+            imgSrc: "assets/images/nrj.png"
+          }
+        },
+
+        {
+          block: "link",
+          targer: "_blank",
+          attrs: {
+            rel: "noreferrer, noopener, nofollow"
+          },
+          url: "https://geometria.ru/spb/",
+          content: {
+            block: "lazyImage",
+            mix: [{ block: "lazyImage", mods: { hidden: true } }],
+            imgSrc: "assets/images/geometria.jpg"
+          }
+        },
+
+        {
+          block: "link",
+          targer: "_blank",
+          attrs: {
+            rel: "noreferrer, noopener, nofollow"
+          },
+          url: "https://kudago.com/spb/",
+          content: {
+            block: "lazyImage",
+            mix: [{ block: "lazyImage", mods: { hidden: true } }],
+            imgSrc: "assets/images/kudago.png"
+          }
+        },
+
+        {
+          block: "link",
+          targer: "_blank",
+          attrs: {
+            rel: "noreferrer, noopener, nofollow"
+          },
+          url: "https://trip2fest.ru/",
+          content: {
+            block: "lazyImage",
+            mix: [{ block: "lazyImage", mods: { hidden: true } }],
+            imgSrc: "assets/images/2fest.png"
+          }
+        }
+      ]
     }
   ];
 });
